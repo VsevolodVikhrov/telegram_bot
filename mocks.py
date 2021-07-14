2522380e90e41e97f82131d41a20b8ca13aa498a
Vsevolod Vikhrov <uapbxxl@gmail.com>
2021-07-15 00:17:14 +0300

    added schedule

--- mocks.py
+++ mocks.py
@@ -2,10 +2,11 @@ masters_list_mock = ["Ihar", "Anton", "Lexa", "Seva"]
 masters_skills_mock = {
     "Ihar": ["Nails", "French"],
     "Anton": ["Break the doors via leg", "Fix domofons"],
-    "Lexa": ["Photohraphy", "Pivo"],
+    "Lexa": ["Photohraphy", "Pivo", "Wine_tasting", "Hang out with extremists"],
     "Seva": ["Coding", "Riding bike"]
                                            }
 dates = ["08.06", "05.04", "03.08", "17.04", "15.12", "12.12", "19.12", "28.01", "12.05", "27.07", "13.05"]
+times = [str(time) for time in range(0, 24)]
 
 
 def get_masters():
@@ -19,3 +20,7 @@ def get_skills(master):
 
 def get_dates(master, skill):
     return dates
+
+
+def get_times(master, skill, date):
+    return times
